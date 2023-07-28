eeaecc367303e7bb41337598bcbff479ba4c1e03
camiyuka <camila.yatabe@gmail.com>
2023-07-28 14:38:08 -0300

    organização das pastas e lista de exercicio 2

---
+++ 6- arrays/lista-de-exercicios/exercicio2.js
@@ -0,0 +1,14 @@
+//2) Crie um programa que seja capaz de percorrer uma lista de números e imprima cada número Par encontrado.
+
+const lista= [21,24,56,84,7,902,23,634,634,89,90,6,43,2];
+
+for (let i = 0; i < lista.length; i ++) {
+    const numero= lista[i]; 
+
+    if(numero%2==0){
+        console.log(numero)
+    } else{
+
+    }
+
+}
